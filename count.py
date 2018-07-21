6ef18cb3c4efc20cbee3ce9a1eaf73a44bbef5a5
Karan Maheshwari <39696067+karanm14@users.noreply.github.com>
2018-07-21 18:52:56 +0800

    Update count.py

--- count.py
+++ count.py
@@ -3,20 +3,36 @@ from PIL import Image
 import pandas as pd
 import xlsxwriter
 
+
+
+##################
 #WE HAVE TWO CSV FILES 1) WITH LABELS 2)WITH INDEX 
 
 df1 = pd.read_csv("Desktop/Book3.csv") #INDEX OF SUBREGIONS WITH 1
 
+
+##################
+
 df2 = pd.read_excel("Desktop/imageskaist1.xlsx") #FILES WITH LABELS 
 
+
+
+##################
 x=[]
 for i in range(0,len(df1)):
     x.append(df1["index"][i]-1)
 
+    
+    
+##################
 z=[]
 for i in x:
     z.append(df2['Label'][i])
 
+    
+    
+
+##################    
 #NOW we will read the boxes of all predictions which are 1 and store them with label 
 
 h=[]
@@ -25,9 +41,13 @@ for i in range(0,len(df1)):
     h.append([z[i],float(df4[0]),float(df4[1]),float(df4[2]),float(df4[3])])
 
 
+##################
 #to check if its working correctly
 print(h[1][0],h[1][1],h[1][2],h[1][3],h[1][4])
 
+
+
+##################
 workbook = xlsxwriter.Workbook('Desktop/kaist72withboxtrainbai.xlsx')
 worksheet = workbook.add_worksheet()
 worksheet.write('A1', 'BaseImage')
